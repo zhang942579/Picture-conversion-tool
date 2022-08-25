c254af6ffdd1486b9165faf05365eb35efd7ca8a
zhang942579 <2252440346@qq.com>
2022-08-25 11:14:25 +0800

    修改

--- Resource/Config.py
+++ Resource/Config.py
@@ -6,7 +6,7 @@
 
 class Config:
     def __init__(self):
-        self.ak = "3ZQF2VL8L3URUUYUSJMQ"
-        self.sk = "NM9WEfdUd4gXNUDC9Mqa6M9Z1rBp969iE7W9wOeL"
+        self.ak = "**"
+        self.sk = "**"
         self.project_id = "	***"
 
